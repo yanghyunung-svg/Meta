186db6d71048fca508c5f9d9a673eaefa71db326
yanghu <yanghyunung@gmail.com>
2025-12-12 17:26:02 +0900

    commit

---
+++ src/main/java/com/meta/mapper/TbStdDmnBscMapper.java
@@ -0,0 +1,25 @@
+package com.meta.mapper;
+
+import com.meta.dto.TbStdDmnBscDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author     : somnus21
+ * @file       : TbStdDmnBscMapper.java
+ * @version    : 1.0.0
+ * @createDate : 2025-12-10 오후 2:33:57
+ * @updateDate : 2025-12-10 오후 2:33:57
+ * @desc       : 표준도메인기본 TB_STD_DMN_BSC 
+ */
+@Mapper
+public interface TbStdDmnBscMapper {
+    public TbStdDmnBscDto getData(TbStdDmnBscDto tbStdDmnBscDto);
+    public TbStdDmnBscDto getLockData(TbStdDmnBscDto tbStdDmnBscDto);
+    public int insertData(TbStdDmnBscDto tbStdDmnBscDto);
+    public int updateData(TbStdDmnBscDto tbStdDmnBscDto);
+    public int deleteData(TbStdDmnBscDto tbStdDmnBscDto);
+    public List<TbStdDmnBscDto> getListData(TbStdDmnBscDto tbStdDmnBscDto);
+    public List<TbStdDmnBscDto> getDmnComboData();
+}
