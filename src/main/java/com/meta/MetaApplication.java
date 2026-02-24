92aee5580d80da9681e45dd926547f94eea047d1
yanghu <yanghyunung@gmail.com>
2026-02-24 15:10:32 +0900

    commit

--- src/main/java/com/meta/MetaApplication.java
+++ src/main/java/com/meta/MetaApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.meta") // 컨트롤러 스캔 보장
+@SpringBootApplication(scanBasePackages ={"com.meta", "com.upbit"}) // 컨트롤러 스캔 보장
 @MapperScan("com.meta.mapper")
 public class MetaApplication {
     public static void main(String[] args) {
